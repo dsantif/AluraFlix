58045ad5d31fbe3da0b51406205a8b012a1fca38
dsantif <dianasantillana24@gmail.com>
2024-07-15 14:22:20 -0600

    update links de videos

--- src/componentes/Banner.js
+++ src/componentes/Banner.js
@@ -3,18 +3,18 @@ import "./Banner.css"
 function Banner(){
     return (
         <div className="banner_container">
-            <div className="card">
-                <div className="texto_card">
+            <div className="banner_card">
+                <div className="texto_banner">
                     <div className="fondo_titulo">
-                    <h1 className="titulo_card">FRONTEND</h1>
+                    <h1 className="titulo_banner">FRONTEND</h1>
                     </div>
-                    <h2 className="subtitulo_card">Challenge React</h2>
-                    <h5 className="descripcion_card">Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</h5>
+                    <h2 className="subtitulo_banner">Challenge React</h2>
+                    <h5 className="descripcion_banner">Este challenge es una forma de aprendizaje. Es un mecanismo donde podrás comprometerte en la resolución de un problema para poder aplicar todos los conocimientos adquiridos en la formación React.</h5>
                 </div>
 
-                <div className="video_card">
-                    <div className="card_img">
-                        <a href="https://www.youtube.com/watch?v=ov7vA5HFe6w&ab_channel=AluraLatam" target="_blank" rel="noopener noreferrer">
+                <div className="video_banner">
+                    <div className="banner_img">
+                        <a href="https://www.youtube.com/watch?v=ov7vA5HFe6w&ab_channel=AluraLatam" target="_blank">
                         <img src="/img/video_card.jpg"/>
                         </a>
                     </div>
--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -8,34 +8,40 @@ function Menu(){
                     <h4>FRONTEND</h4>
                 </div>
 
-                <div className="cards_front">
-                    <div className="box_front">
-                        <div className="img_front">
+                <div className="cards_container">
+                    <div className="cards_box">
+                        <div className="front_img">
+                            <a href="https://youtu.be/PztCEdIJITY?si=YtfFzB3PEW_I9w4O" target="blank">
                             <div><img src="/img/vf1.jpg"/></div>
+                            </a>
                         </div>
-                        <div className="btn_front">
-                            <button className="btn_borrar"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
-                            <button className="btn_editar"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_f"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_f"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
                         </div>
                     </div>
 
-                    <div className="box_front">
-                    <div className="img_front">
+                    <div className="cards_box">
+                        <div className="front_img">
+                            <a href="https://youtu.be/GJfOSoaXk4s?si=ouMBfDIlgxN4yQn3" target="blank">                        
                             <div><img src="/img/vf2.jpg"/></div>
+                            </a>
                         </div>
-                        <div className="btn_front">
-                            <button className="btn_borrar"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
-                            <button className="btn_editar"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_f"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_f"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
                         </div>
                     </div>
 
-                    <div className="box_front">
-                    <div className="img_front">
+                    <div className="cards_box">
+                        <div className="front_img">
+                            <a href="https://www.youtube.com/watch?v=rpvrLaBQwgg" target="blank">
                             <div><img src="/img/vf3.jpg"/></div>
+                            </a>
                         </div>
-                        <div className="btn_front">
-                            <button className="btn_borrar"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
-                            <button className="btn_editar"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_f"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_f"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
                         </div>
                     </div>
 
@@ -46,7 +52,42 @@ function Menu(){
                 <div className="titulo_back">
                     <h4>BACKEND</h4>
                 </div>
-                <div className="cards_back">
+                <div className="cards_container">
+                    <div className="cards_box">
+                        <div className="back_img">
+                            <a href="https://www.youtube.com/watch?v=5XPojnx9bb8" target="blank">
+                            <div><img src="/img/vb1.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_b"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_b"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
+
+                    <div className="cards_box">
+                        <div className="back_img">
+                            <a href="https://www.youtube.com/watch?v=aure5d3B88g" target="blank">
+                            <div><img src="/img/vb2.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_b"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_b"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
+
+                    <div className="cards_box">
+                        <div className="back_img">
+                            <a href="https://www.youtube.com/watch?v=EoPvlE85XAQ" target="blank">
+                            <div><img src="/img/vb3.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_b"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_b"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
 
                 </div>
 
@@ -56,7 +97,42 @@ function Menu(){
                 <div className="titulo_innovacion">
                     <h4>INNOVACION Y GESTION</h4>
                 </div>
-                <div className="cards_innovacion">
+                <div className="cards_container">
+                    <div className="cards_box">
+                        <div className="inn_img">
+                            <a href="https://www.youtube.com/watch?v=vhwspfvI52k" target="blank">
+                            <div><img src="/img/vi1.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_i"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_i"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
+
+                    <div className="cards_box">
+                        <div className="inn_img">
+                            <a href="https://www.youtube.com/watch?v=YhR7Zp8NUzE" target="blank">
+                            <div><img src="/img/vi2.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_i"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_i"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
+
+                    <div className="cards_box">
+                        <div className="inn_img">
+                            <a href="https://www.youtube.com/watch?v=6N3OkLCfK-0" target="blank">
+                            <div><img src="/img/vi3.jpg"/></div>
+                            </a>
+                        </div>
+                        <div className="btn_cards">
+                            <button className="btn_borrar_i"><i class="fi fi-rr-delete-right"></i>Eliminar</button>
+                            <button className="btn_editar_i"><i class="fi fi-rr-comment-alt-edit"></i>Editar</button>
+                        </div>
+                    </div>
 
                 </div>
 
