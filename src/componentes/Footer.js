9e34360a97113dc2c53acd3cf6ec06250f402dce
dsantif <dianasantillana24@gmail.com>
2024-07-15 15:15:45 -0600

    update footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -3,6 +3,7 @@ import "./Footer.css"
 function Footer(){
     return <footer>
         <img className="footer_img" src="/img/logo.png"/>
+        <p className="nombre_footer">Diana Santillana 2024</p>
     </footer>
 }
 
